5b9294cdc198615d215e0ef2fdfd45726ee481d6
selome <47634241+selome-git@users.noreply.github.com>
2021-06-23 15:42:53 +0300

    Update JavaWithAnt.java

--- src/javawithant/JavaWithAnt.java
+++ src/javawithant/JavaWithAnt.java
@@ -18,20 +18,20 @@ public class JavaWithAnt {
     	 * thats why I have created the array of size 6. You can
     	 * change this as per the requirement.
     	 */
-        int marks[] = new int[6];
+        int marks[] = new int[5];
         int i;
         float total=0, avg;
         Scanner scanner = new Scanner(System.in);
 		
         
-        for(i=0; i<6; i++) { 
+        for(i=0; i<5; i++) { 
            System.out.print("Enter Marks of Subject"+(i+1)+":");
            marks[i] = scanner.nextInt();
            total = total + marks[i];
         }
         scanner.close();
         //Calculating average here
-        avg = total/6;
+        avg = total/5;
         System.out.print("The student Grade is: ");
         if(avg>=80)
         {
@@ -45,9 +45,12 @@ public class JavaWithAnt {
         {
             System.out.print("C");
         }
-        else
+         else if(avg>=35 && avg<39)
         {
             System.out.print("D");
+        else
+        {
+            System.out.print("F");
         }
     }
 }
